c6a69ae4376374aa585292a36dd901471872d134
Won Beom Kim <shepelt@gmail.com>
2020-11-25 22:09:30 +0900

    inverted spindle control bit for correct behavior

--- grbl/config.h
+++ grbl/config.h
@@ -273,7 +273,7 @@
 
 // Inverts the spindle enable pin from low-disabled/high-enabled to low-enabled/high-disabled. Useful
 // for some pre-built electronic boards.
-// #define INVERT_SPINDLE_ENABLE_PIN // Default disabled. Uncomment to enable.
+#define INVERT_SPINDLE_ENABLE_PIN // Default disabled. Uncomment to enable.
 
 // Inverts the selected coolant pin from low-disabled/high-enabled to low-enabled/high-disabled. Useful
 // for some pre-built electronic boards.
